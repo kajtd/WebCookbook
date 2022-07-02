906639dc901bf4d5e62245e89813d7dd6ab3af10
Kajetan Domagala <kajtekd8@gmail.com>
2022-07-02 21:29:11 +0200

    feat: add two atom components - AppButton and AppInput

---
+++ src/components/atoms/AppButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button
+    :class="[
+      'border-2 border-gray-900 bg-primary text-gray-100 font-semibold py-2 px-6 rounded-lg text-sm flex items-center justify-center',
+      additionalClass
+    ]"
+    :type="type"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  additionalClass?: string
+  size?: string
+  type?: 'button' | 'submit' | 'reset'
+}>()
+</script>
---
+++ src/components/atoms/AppInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <input
+    :id="id"
+    :ref="ref"
+    :type="type"
+    :value="modelValue"
+    :placeholder="placeholder"
+    :class="[
+      ' text-sm text-gray-900  border-2 rounded-lg border-gray-800 mb-3 py-2 px-3  font-semibold placeholder:text-sm placeholder:text-gray-900 placeholder:font-semibold focus:outline-none focus:border-primary',
+      { 'pointer-events-none opacity-50': disabled }
+    ]"
+    :required="required"
+    @input="$emit('update:modelValue', ($event.target  as HTMLInputElement).value)"
+  />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  id: string
+  ref?: string
+  type?: string
+  modelValue: string
+  placeholder?: string
+  disabled?: boolean
+  required?: boolean
+}>()
+defineEmits(['update:modelValue'])
+</script>
