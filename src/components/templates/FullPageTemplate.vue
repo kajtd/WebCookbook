78357308fe57fb6e1c07e4e4819e4a60fb1a8b7e
Kajetan Domagala <kajtekd8@gmail.com>
2022-07-05 12:54:57 +0200

    feat(fullpagetemplate): create page template for future pages

---
+++ src/components/templates/FullPageTemplate.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-col justify-between items-center min-h-screen w-full">
+    <HeaderNavigation @toggle-login-popup="toggleLoginPopup" />
+    <slot />
+    <AppFooter @toggle-login-popup="toggleLoginPopup" />
+    <AppPopup :popup-visible="loginPopupVisible" @toggle-popup="toggleLoginPopup">
+      <LoginForm />
+    </AppPopup>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import HeaderNavigation from './../organisms/HeaderNavigation.vue'
+import AppFooter from './../organisms/AppFooter.vue'
+import AppPopup from '../global/AppPopup.vue'
+import LoginForm from '../molecules/LoginForm.vue'
+
+const loginPopupVisible = ref(false)
+
+const toggleLoginPopup = (): void => {
+  loginPopupVisible.value = !loginPopupVisible.value
+}
+</script>
