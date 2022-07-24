ab6c6bc4b123da4f5ab7b106fd4101903fb219e1
Kajetan Domagala <kajtekd8@gmail.com>
2022-07-24 17:32:51 +0200

    feat: add dropdown with logout button

--- src/components/atoms/AppDropdown.vue
+++ src/components/atoms/AppDropdown.vue
@@ -2,21 +2,26 @@
   <div ref="container" class="absolute right-2 top-2 bg-white rounded-lg shadow-lg" tabindex="0">
     <div
       v-if="open"
-      class="text-base divide-y-2 divide-gray-900 z-30 shadow-lg rounded-lg"
+      class="text-base"
       role="menu"
       aria-orientation="vertical"
       v-click-outside="handleClickOutside"
       aria-labelledby="options-menu"
     >
-      <button
-        v-for="(item, index) of items"
-        :key="index"
-        :class="['flex items-center justify-center w-full px-4 py-1', item.additionalClass]"
-        @click="handleItemClick(item)"
-      >
-        <span class="font-semibold my-1">{{ item.label }}</span>
-        <Icon v-if="item.icon" :icon="item.icon" class="ml-2" />
-      </button>
+      <div v-if="items" class="divide-y-2 divide-gray-900 z-30 shadow-lg rounded-lg">
+        <button
+          v-for="(item, index) of items"
+          :key="index"
+          :class="['flex items-center justify-center w-full px-4 py-1', item.additionalClass]"
+          @click="handleItemClick(item)"
+        >
+          <span class="font-semibold my-1">{{ item.label }}</span>
+          <Icon v-if="item.icon" :icon="item.icon" class="ml-2" />
+        </button>
+      </div>
+      <div v-else class="bg-white z-30 shadow-lg rounded-lg">
+        <slot />
+      </div>
     </div>
   </div>
 </template>
@@ -33,7 +38,7 @@ interface Item {
 
 defineProps<{
   open: boolean
-  items: Item[]
+  items?: Item[]
 }>()
 const emit = defineEmits(['close'])
 
--- src/components/organisms/HeaderNavigation.vue
+++ src/components/organisms/HeaderNavigation.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="flex items-center justify-between w-full mx-auto px-4 md:px-8 py-6 bg-orangeLight border-b-4 border-black z-[3]"
+    class="sticky top-0 flex items-center justify-between w-full mx-auto px-4 md:px-8 py-6 bg-orangeLight border-b-4 border-black z-[3]"
   >
     <div class="max-w-5xl w-full mx-auto flex items-center justify-between">
       <AppLogo />
@@ -16,11 +16,23 @@
           >
             Login
           </AppButton>
-          <img
-            v-else-if="store.user.photoURL"
-            :src="store.user.photoURL"
-            class="rounded-full h-10 w-10 border-2 border-black"
-          />
+          <div v-else class="relative w-full">
+            <button @click="logoutDropdownVisible = true">
+              <img
+                v-if="store.user.photoURL"
+                :src="store.user.photoURL"
+                class="rounded-full h-10 w-10 border-2 border-black"
+              />
+            </button>
+            <AppDropdown :open.sync="logoutDropdownVisible" @close="closeDropdown">
+              <div class="gap-2 w-full px-8 py-4">
+                <p class="font-semibold text-base pb-2 border-b-2 border-black mb-5 text-center">
+                  {{ store.user.displayName }}
+                </p>
+                <AppButton @click="logout">Logout</AppButton>
+              </div>
+            </AppDropdown>
+          </div>
         </ul>
       </nav>
       <transition name="mobile-nav">
@@ -60,14 +72,18 @@ import AppButton from '../atoms/AppButton.vue'
 import HeaderNavigationLink from '../atoms/HeaderNavigationLink.vue'
 import HamburgerMenuButton from '../atoms/HamburgerMenuButton.vue'
 import AppLogo from '../atoms/AppLogo.vue'
+import AppDropdown from '../atoms/AppDropdown.vue'
 import { useStore } from './../../store/index'
 import Link from './../../types/Link'
+import { signOut } from 'firebase/auth'
+import { auth } from './../../firebase'
 
 defineEmits(['toggleLoginPopup'])
 
 const store = useStore()
 
 const mobileNav = ref(false)
+const logoutDropdownVisible = ref(false)
 
 const links = ref<Link[]>([
   {
@@ -83,6 +99,17 @@ const links = ref<Link[]>([
     url: '/about'
   }
 ])
+
+const closeDropdown = (): void => {
+  logoutDropdownVisible.value = false
+}
+
+const logout = (): void => {
+  signOut(auth).then(() => {
+    closeDropdown()
+    window.location.reload()
+  })
+}
 
 const toggleMobileNav = (): void => {
   mobileNav.value = !mobileNav.value
