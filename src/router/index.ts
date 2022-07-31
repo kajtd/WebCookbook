18666ffc11d07d05162bc8c22a5a1db21a531483
Kajetan Domagala <kajtekd8@gmail.com>
2022-07-31 15:28:25 +0200

    refactor: remove unnecessary middleware

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
-import checkAuth from '../middleware/checkAuth'
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'Home',
-    beforeEnter: checkAuth,
     component: () => import('../views/Home.vue')
   },
   {
